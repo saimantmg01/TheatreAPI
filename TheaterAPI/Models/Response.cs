3a37abb92f19bf005f9a94be4fdcddbe5c26fae6
saimantmg01 <saimantmg01@gmail.com>
2022-05-02 20:09:55 -0400

    working response model

---
+++ TheaterAPI/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TheaterAPI.Models
+{
+	public class Response
+	{
+		public int StatusCodes { get; set; }
+		public string StatusDescription { get; set; }
+		public List<Movie> Movies { get; set; } = new();
+		public List<Theatre> Theatres { get; set; } = new();
+	}
+}
+
+
